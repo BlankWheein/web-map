2392d59cc7fa621a8d814d9b1a4e800b84ddd83a
Blank <47646799+BlankWheein@users.noreply.github.com>
2023-12-24 21:59:13 +0100

    Added stats

--- index.js
+++ index.js
@@ -23,9 +23,24 @@ app.get('/',  function(req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+app.get('/stats',  function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/stats.html'));
+});
+
+app.get('/api/clickrate', async function(req, res) {
+  var data = await client.query(`SELECT a.id, a.name, a.url, count(ac) filter(where ac.type=0) as x, count(ac) filter(where ac.type=1) as y  FROM map.activities a
+  left outer join map.activity_clicked ac on ac.activity_id=a.id
+  group by a.id;`);
+  res.send(
+    {
+        data : data.rows
+    }
+  );
+});
+
 
 app.post('/api/activities', async function(req, res) {
-  //var data = await client.query(`INSERT INTO map.activities(id, lat, "long", name) VALUES ('${v4()}',${req.body.lat}, ${req.body.lng}, 'Auto Generated');`);
+  var data = await client.query(`INSERT INTO map.activities(id, lat, "long", name, url, price_child, price_adult, address, opening_hours, offer) VALUES ('${v4()}',${req.body.lat}, ${req.body.lng}, 'Auto Generated', 'https://www.facebook.com', 50, 150, 'Danmarksgade 123', '<span>Mandag-Fredag: </span> <span>08:00-17:00</span> <br/><span>Lørdag: </span> <span>10:00-21:00</span> <br/><span>Søndag: </span> <span>Lukket</span>', '<span>Rabatkode: fiktivkode01</span><br/><span>Få 10% rabat</span>' );`);
   res.send();
 });
 
