dac27444bbeea1ebb73570c7609e4b2163a8212e
Blank <47646799+BlankWheein@users.noreply.github.com>
2023-12-24 13:27:17 +0100

    added markers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path')
 const { Client } = require('pg')
+const {v4} = require("uuid");
 const client = new Client({
   user: 'postgres',
   host: 'localhost',
@@ -14,20 +15,28 @@ client.connect(function(err) {
 });
 
 const app = express();
+app.use(express.json())
 app.use('/public', express.static('public'));
 app.use('/dist', express.static('dist'));
 
 app.get('/',  function(req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
-  });
+});
 
-  app.get('/api/activities', async function(req, res) {
-    var data = await client.query("SELECT * FROM map.activities")
-    res.send(
-        {
-            data : data.rows
-        }
-    );
-  });
+
+app.post('/api/activities', async function(req, res) {
+  console.log(req.body);
+  var data = await client.query(`INSERT INTO map.activities(id, lat, "long", name) VALUES ('${v4()}',${req.body.lat}, ${req.body.lng}, 'Auto Generated');`);
+  res.send(data);
+});
+
+app.get('/api/activities', async function(req, res) {
+  var data = await client.query("SELECT * FROM map.activities")
+  res.send(
+      {
+          data : data.rows
+      }
+  );
+});
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
